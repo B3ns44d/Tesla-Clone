613961146cba60e99ad2582c3a181e403dcf8f63
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-01-28 02:22:18 +0100

    create tesla site header with route

---
+++ src/components/Header.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Header.css";
+import Logo from "../assets/teslaLogoSmall.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="headerLogo">
+        <Link to={"/"}>
+          <img src={Logo} alt="Tesla Logo" />
+        </Link>
+      </div>
+      <Router>
+        <div className="headerCenter">
+          <Link to={"/models"} className="headerText">
+            Model S
+          </Link>
+          <Link to={"/models3"} className="headerText">
+            Model 3
+          </Link>
+          <Link to={"/modelx"} className="headerText">
+            Model X
+          </Link>
+          <Link to={"/modely"} className="headerText">
+            Model Y
+          </Link>
+          <Link to={"/solarroof"} className="headerText">
+            Solar Roof
+          </Link>
+          <Link to={"/solarpanels"} className="headerText">
+            Solar Panels
+          </Link>
+        </div>
+        <div className="headerRight">
+          <Link to={"/shop"} className="headerText">
+            Shop
+          </Link>
+          <Link to={"/teslaaccount"} className="headerText">
+            Tesla Account
+          </Link>
+          <div className="menuIcon">{/* <MenuIcon /> */}</div>
+        </div>
+        <Switch>
+          <Route path="/">
+            {/* <App /> */}
+          </Route>
+          <Route path="/models">
+            {/* <Models /> */}
+          </Route>
+          <Route path="/models3">
+            {/* <Models3 /> */}
+          </Route>
+          <Route path="/modelx">
+            {/* <Modelsx /> */}
+          </Route>
+          <Route path="/modely">
+            {/* <Modely /> */}
+          </Route>
+          <Route path="/solarroof">
+            {/* <Solarroof /> */}
+          </Route>
+          <Route path="/solarpanels">
+            {/* <Solarpanels /> */}
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default Header;
