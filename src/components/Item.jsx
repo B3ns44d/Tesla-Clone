c7ae2ba1adb2bb10984e73836ba0a42d2926ec4e
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-01-29 14:25:16 +0100

    done wiht item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,21 +1,45 @@
 import React from "react";
+import Button from "./Button";
 import "./Item.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 function Item({
   title,
-  desc
+  desc,
+  descLink,
+  backgroundImg,
+  leftBtnTxt,
+  leftBtnLink,
+  rightBtnTxt,
+  rightBtnLink,
+  twoButtons,
+  first,
 }) {
   return (
-    <div className="item" style={{}}>
+    <div
+      className="item"
+      style={{
+        backgroundImage: `url(${backgroundImg})`,
+      }}
+    >
       <div className="itemContainer">
-        <div className="itemTitle">
+        <div className="itemText">
           <p>{title}</p>
           <div className="itemDescription">
             <p>{desc}</p>
           </div>
         </div>
         <div className="itemLowerThird">
-          <div className="itemButtons"></div>
-          <div className="expandMoreIcon">{/* Expand More Icon */}</div>
+          <div className="itemButtons">
+            <Button imp="primary" text={leftBtnTxt} link={leftBtnLink} />
+            {twoButtons && (
+              <Button imp="secondary" text={rightBtnTxt} link={rightBtnLink} />
+            )}
+          </div>
+          {first && (
+            <div className="expandMoreIcon">
+              <ExpandMoreIcon />
+            </div>
+          )}
         </div>
       </div>
     </div>
